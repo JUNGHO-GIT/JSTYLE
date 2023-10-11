7dfc3ae077a48b7b9857f08a378e6029159e53d4
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-10-11 20:14:46 +0900

    Create Jstyle_extra.js

---
+++ Jstyle_extra.js
@@ -0,0 +1,120 @@
+// --------------------------------------------------------------------------------------------->
+var dynamicStyle = function () {
+  // 1. Object mapping style prefixes to corresponding CSS property and its unit
+  const stylesNumber = {
+    w: ["width", "%"],
+    h: ["height", "%"],
+    p: ["padding", "px"],
+    pt: ["padding-top", "px"],
+    pb: ["padding-bottom", "px"],
+    ps: ["padding-left", "px"],
+    pe: ["padding-right", "px"],
+    m: ["margin", "px"],
+    mt: ["margin-top", "px"],
+    mb: ["margin-bottom", "px"],
+    ms: ["margin-left", "px"],
+    me: ["margin-right", "px"],
+    fw: ["font-weight", "00"],
+    fs: ["font-size", "px"],
+  };
+
+  const stylesString = {
+    "d-center": {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+    },
+    "d-left": {
+      display: "flex",
+      justifyContent: "flex-start",
+      alignItems: "center",
+      textAlign: "left",
+    },
+    "d-right": {
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "center",
+      textAlign: "right",
+    },
+  };
+
+  // 2. Function to validate if the given className has a valid style prefix
+  function checkValidClass(className) {
+    if (stylesString[className]) {
+      return true;
+    }
+
+    const parts = className.split("-");
+    const prefix = parts[0];
+    const value = parts[1];
+    return (
+      stylesNumber[prefix] &&
+      !isNaN(value) &&
+      parts.length === 2 &&
+      Object.hasOwnProperty.call(stylesNumber, prefix)
+    );
+  }
+
+  // 3. Function to apply styles to elements with valid className
+  function applyStyle(element, className) {
+    if (stylesString[className]) {
+      for (let prop in stylesString[className]) {
+        element.style[prop] = stylesString[className][prop];
+      }
+    } else {
+      const parts = className.split("-");
+      const prefix = parts[0];
+      const value = parts[1];
+      if (stylesNumber[prefix] && !isNaN(value) && parts.length === 2) {
+        const styleInfo = stylesNumber[prefix];
+        element.style.setProperty(
+          styleInfo[0],
+          value + styleInfo[1],
+          "important"
+        );
+      }
+    }
+  }
+
+  // 4. Construct DOM selectors based on supported style prefixes
+  var numberSelector = Object.keys(stylesNumber)
+    .map(function (prefix) {
+      return '[class*="' + prefix + '-"][class*="' + prefix + '-\\d"]';
+    })
+    .join(", ");
+
+  var stringSelector = Object.keys(stylesString)
+    .map(function (prefix) {
+      return "." + prefix;
+    })
+    .join(", ");
+
+  var selector = numberSelector + ", " + stringSelector;
+
+  // 5. Select elements in the document using the constructed selectors
+  var elements = document.querySelectorAll(selector);
+
+  // 6. Iterate through selected elements and apply dynamic styles
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var classNames = [];
+
+    if (typeof element.className === "string") {
+      classNames = element.className.split(" ");
+    } else if (element.className instanceof SVGAnimatedString) {
+      classNames = element.className.baseVal.split(" ");
+    }
+    for (var j = 0; j < classNames.length; j++) {
+      var className = classNames[j];
+      if (checkValidClass(className)) {
+        applyStyle(element, className);
+      }
+    }
+  }
+};
+
+// 7. Invoke the dynamic styling function after window finishes loading (used `requestAnimationFrame`)
+window.addEventListener("load", () => {
+  requestAnimationFrame(dynamicStyle);
+});
