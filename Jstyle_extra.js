8ff325f3d34c0f35f8c334b8aaad6e57198ba003
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-10-11 22:04:16 +0900

    Update Jstyle_extra.js

--- Jstyle_extra.js
+++ Jstyle_extra.js
@@ -1,41 +1,41 @@
 // --------------------------------------------------------------------------------------------->
-var dynamicStyle = function () {
+var dynamicStyle = function (baseElement = document) {
   // 1. Object mapping style prefixes to corresponding CSS property and its unit
   const stylesNumber = {
-    w: ["width", "%"],
-    h: ["height", "%"],
-    p: ["padding", "px"],
-    pt: ["padding-top", "px"],
-    pb: ["padding-bottom", "px"],
-    ps: ["padding-left", "px"],
-    pe: ["padding-right", "px"],
-    m: ["margin", "px"],
-    mt: ["margin-top", "px"],
-    mb: ["margin-bottom", "px"],
-    ms: ["margin-left", "px"],
-    me: ["margin-right", "px"],
-    fw: ["font-weight", "00"],
-    fs: ["font-size", "px"],
+    "w": ["width", "%"],
+    "h": ["height", "%"],
+    "p": ["padding", "px"],
+    "pt": ["padding-top", "px"],
+    "pb": ["padding-bottom", "px"],
+    "ps": ["padding-left", "px"],
+    "pe": ["padding-right", "px"],
+    "m": ["margin", "px"],
+    "mt": ["margin-top", "px"],
+    "mb": ["margin-bottom", "px"],
+    "ms": ["margin-left", "px"],
+    "me": ["margin-right", "px"],
+    "fw": ["font-weight", "00"],
+    "fs": ["font-size", "px"],
   };
 
   const stylesString = {
     "d-center": {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      textAlign: "center",
+      "display": "flex",
+      "justifyContent": "center",
+      "alignItems": "center",
+      "textAlign": "center",
     },
     "d-left": {
-      display: "flex",
-      justifyContent: "flex-start",
-      alignItems: "center",
-      textAlign: "left",
+      "display": "flex",
+      "justifyContent": "flex-start",
+      "alignItems": "center",
+      "textAlign": "left",
     },
     "d-right": {
-      display: "flex",
-      justifyContent: "flex-end",
-      alignItems: "center",
-      textAlign: "right",
+      "display": "flex",
+      "justifyContent": "flex-end",
+      "alignItems": "center",
+      "textAlign": "right",
     },
   };
 
@@ -51,8 +51,7 @@ var dynamicStyle = function () {
     return (
       stylesNumber[prefix] &&
       !isNaN(value) &&
-      parts.length === 2 &&
-      Object.hasOwnProperty.call(stylesNumber, prefix)
+      parts.length === 2
     );
   }
 
@@ -62,35 +61,35 @@ var dynamicStyle = function () {
       for (let prop in stylesString[className]) {
         element.style[prop] = stylesString[className][prop];
       }
-    } else {
-      const parts = className.split("-");
-      const prefix = parts[0];
-      const value = parts[1];
-      if (stylesNumber[prefix] && !isNaN(value) && parts.length === 2) {
-        const styleInfo = stylesNumber[prefix];
-        element.style.setProperty(
-          styleInfo[0],
-          value + styleInfo[1],
-          "important"
-        );
-      }
+      return;
+    }
+
+    // If not a string style, check and apply number styles
+    const parts = className.split("-");
+    const prefix = parts[0];
+    const value = parts[1];
+    if (stylesNumber[prefix] && !isNaN(value) && parts.length === 2) {
+      const styleInfo = stylesNumber[prefix];
+      element.style.setProperty(
+        styleInfo[0],
+        value + styleInfo[1],
+        "important"
+      );
     }
   }
 
   // 4. Construct DOM selectors based on supported style prefixes
-  var numberSelector = Object.keys(stylesNumber)
-    .map(function (prefix) {
-      return '[class*="' + prefix + '-"][class*="' + prefix + '-\\d"]';
-    })
-    .join(", ");
+  var numberSelector = Object.keys(stylesNumber).flatMap(function (prefix) {
+    return `[class*=${prefix}-]`;
+  })
+  .join(", ");
 
-  var stringSelector = Object.keys(stylesString)
-    .map(function (prefix) {
-      return "." + prefix;
-    })
-    .join(", ");
+  var stringSelector = Object.keys(stylesString).flatMap(function (prefix) {
+    return `.${prefix}`;
+  })
+  .join(", ");
 
-  var selector = numberSelector + ", " + stringSelector;
+  var selector = `${numberSelector}, ${stringSelector}`;
 
   // 5. Select elements in the document using the constructed selectors
   var elements = document.querySelectorAll(selector);
@@ -98,11 +97,12 @@ var dynamicStyle = function () {
   // 6. Iterate through selected elements and apply dynamic styles
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
-    var classNames = [];
+    var classNames = element.className;
 
     if (typeof element.className === "string") {
       classNames = element.className.split(" ");
-    } else if (element.className instanceof SVGAnimatedString) {
+    }
+    else if (element.className instanceof SVGAnimatedString) {
       classNames = element.className.baseVal.split(" ");
     }
     for (var j = 0; j < classNames.length; j++) {
@@ -112,9 +112,31 @@ var dynamicStyle = function () {
       }
     }
   }
+
+  // Observe dynamic changes and apply styles
+  const observer = new MutationObserver(mutations => {
+    for (let mutation of mutations) {
+      if (mutation.type === "childList") {
+        for (let addedNode of mutation.addedNodes) {
+          if (addedNode.nodeType === Node.ELEMENT_NODE) {
+            dynamicStyle(addedNode);
+          }
+        }
+      }
+    }
+  });
+
+  observer.observe(baseElement, {
+    attributes: false,
+    childList: true,
+    subtree: true,
+  });
 };
 
-// 7. Invoke the dynamic styling function after window finishes loading (used `requestAnimationFrame`)
-window.addEventListener("load", () => {
-  requestAnimationFrame(dynamicStyle);
-});
+// 7. Add event listener for DOMContentLoaded event
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", dynamicStyle);
+}
+else {
+  dynamicStyle();
+}
