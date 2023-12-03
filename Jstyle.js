e8777536343912a333f184f08b13f94b582f984f
JUNGHO-GIT <107819345+JUNGHO-GIT@users.noreply.github.com>
2023-12-04 01:23:32 +0900

    Update Jstyle.js

--- Jstyle.js
+++ Jstyle.js
@@ -1,27 +1,27 @@
-var dynamicStyle = function (baseElement = document) {
+// 1. fnDynamicStyle ------------------------------------------------------------------------------>
+var fnDynamicStyle = function (baseElement = document) {
 
-  // 숫자관련 스타일과 문자열 관련 스타일 --------------------------------------------------------->
+  // 1-1. style 접두사 + css속성 + 단위
   var stylesNumber = {
-    "w": ["width", "%"],
-    "h": ["height", "%"],
-    "t": ["top", "px"],
-    "b": ["bottom", "px"],
-    "l": ["left", "px"],
-    "r": ["right", "px"],
-    "p": ["padding", "px"],
-    "pt": ["padding-top", "px"],
-    "pb": ["padding-bottom", "px"],
-    "ps": ["padding-left", "px"],
-    "pe": ["padding-right", "px"],
-    "m": ["margin", "px"],
-    "mt": ["margin-top", "px"],
-    "mb": ["margin-bottom", "px"],
-    "ms": ["margin-left", "px"],
-    "me": ["margin-right", "px"],
-    "fw": ["font-weight", "00"],
-    "fs": ["font-size", "px"],
+    w  : ["width", "%"],
+    h  : ["height", "%"],
+    p  : ["padding", "px"],
+    pt : ["padding-top", "px"],
+    pb : ["padding-bottom", "px"],
+    ps : ["padding-left", "px"],
+    pe : ["padding-right", "px"],
+    m  : ["margin", "px"],
+    mt : ["margin-top", "px"],
+    mb : ["margin-bottom", "px"],
+    ms : ["margin-left", "px"],
+    me : ["margin-right", "px"],
+    fw : ["font-weight", "00"],
+    fs : ["font-size", "px"],
+    fsr : ["font-size", "rem"],
+    z : ["z-index", ""],
   };
 
+  // 1-2. style 접두사 + css속성
   var stylesString = {
     "d-center": {
       "display": "flex",
@@ -47,12 +47,18 @@ var dynamicStyle = function (baseElement = document) {
     "d-flex": {
       "display": "flex",
     },
-    "d-inline-block": {
+    "d-inline": {
       "display": "inline-block",
     },
     "d-inline-flex": {
       "display": "inline-flex",
     },
+    "pos-static": {
+			"position": "static",
+		},
+		"pos-relative": {
+			"position": "relative",
+		},
     "over-hidden": {
       "overflow": "hidden",
     },
@@ -61,6 +67,7 @@ var dynamicStyle = function (baseElement = document) {
     },
     "pointer": {
       "cursor": "pointer",
+      "caret-color": "transparent",
     },
     "resize-none": {
       "resize": "none",
@@ -69,118 +76,152 @@ var dynamicStyle = function (baseElement = document) {
       "width": "-webkit-fill-available",
       "height": "-webkit-fill-available",
     },
-    "pos-rel": {
-      "position": "relative",
+    "fw-normal": {
+      "font-weight": "normal",
     },
-    "pos-ab": {
-      "position": "absolute",
+    "fw-bold": {
+      "font-weight": "bold",
     },
-    "pos-fix": {
-      "position": "fixed",
-    },
-    "pos-stc": {
-      "position": "static",
+    "fw-bolder": {
+      "font-weight": "bolder",
     },
   };
 
-  // 스타일 맵 만들기 ----------------------------------------------------------------------------->
-  var styleMap = new Map();
-
-  Object.keys(stylesNumber).forEach((key) => {
-    styleMap.set(key, {
-      type: "number",
-      styles: stylesNumber[key],
-    });
-  });
-
-  Object.keys(stylesString).forEach((key) => {
-    styleMap.set(key, {
-      type: "string",
-      styles: stylesString[key],
-    });
-  });
-
-  // 숫자 검증 및 적용 ---------------------------------------------------------------------------->
-  var applyNumberStyle = (element, className) => {
-    var [prefix, value] = className.split("-");
-    var styleInfo = styleMap.get(prefix);
-
-    if (styleInfo && styleInfo.type === "number") {
-      var [property, unit] = styleInfo.styles;
-      var style = value + unit;
-      element.style.setProperty(property, style, "important");
+  // 2. 정의한 속성에 존재하는 클래스 이름인지 확인
+  function checkValidClass(className) {
+    if (stylesString[className]) {
+      return true;
     }
-  };
 
-  // 문자열 검증 및 적용 -------------------------------------------------------------------------->
-  var applyStringStyle = (element, className) => {
-    var styleInfo = styleMap.get(className);
-    if (styleInfo && styleInfo.type === "string") {
-      var style = styleInfo.styles;
-      Object.keys(style).forEach((property) => {
-        element.style.setProperty(property, style[property], "important");
-      });
+    var parts = className.split("-");
+    var prefix = parts[0];
+    var value = parts[1];
+
+    if (value && value.startsWith("n")) {
+      // Remove 'n'
+      value = value.substring(1);
     }
-  };
 
-  // 전체 스타일 검증 및 해당 함수 호출 ----------------------------------------------------------->
-  var checkAndApplyStyle = (element) => {
-    var classNames = [];
+    return (
+      stylesNumber[prefix] &&
+      !isNaN(value) &&
+      parts.length === 2
+    );
+  }
+
+  // 3. 스타일 적용
+  function applyStyle(element, className) {
+    if (stylesString[className]) {
+      for (let prop in stylesString[className]) {
+        element.style[prop] = stylesString[className][prop];
+      }
+      return;
+    }
+
+    var parts = className.split("-");
+    var prefix = parts[0];
+    var value = parts[1];
+
+    if (value && value.startsWith("n")) {
+      value = "-" + value.substring(1);
+    }
+
+    if (stylesNumber[prefix] && !isNaN(value) && parts.length === 2) {
+      var styleInfo = stylesNumber[prefix];
+      element.style.setProperty(
+        styleInfo[0],
+        value + styleInfo[1],
+        "important"
+      );
+    }
+  }
+
+  // 4. style 접두사를 기반으로 css 선택자 생성
+  var numberSelector = Object.keys(stylesNumber).flatMap(function (prefix) {
+    return `[class*=${prefix}-]`;
+  })
+  .join(", ");
+
+  var stringSelector = Object.keys(stylesString).flatMap(function (prefix) {
+    return `.${prefix}`;
+  })
+  .join(", ");
+
+  // 5. 선택자를 기반으로 DOM 요소를 가져옴
+  var selector = `${numberSelector}, ${stringSelector}`;
+  var elements = document.querySelectorAll(selector);
+
+  // 6. 가져온 요소에 스타일 적용
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var classNames = element.className;
 
     if (typeof element.className === "string") {
-      classNames = element.className.split(" ").filter(Boolean);
+      classNames = element.className.split(" ");
     }
-    else {
-      classNames = Array.from(element.classList);
+    else if (element.className instanceof SVGAnimatedString) {
+      classNames = element.className.baseVal.split(" ");
     }
-
-    classNames.forEach((className) => {
-      if (styleMap.has(className)) {
-        var styleInfo = styleMap.get(className);
-        if (styleInfo) {
-          if (styleInfo.type === "number") {
-            applyNumberStyle(element, className);
-          } else if (styleInfo.type === "string") {
-            applyStringStyle(element, className);
-          }
-        }
+    for (var j = 0; j < classNames.length; j++) {
+      var className = classNames[j];
+      if (checkValidClass(className)) {
+        applyStyle(element, className);
       }
-      else {
-        var stylePrefix = className.split("-")[0];
-        if (styleMap.has(stylePrefix)) {
-          var styleInfo = styleMap.get(stylePrefix);
-          if (styleInfo && styleInfo.type === "number") {
-            applyNumberStyle(element, className);
-          }
-        }
-      }
-    });
-  };
+    }
+  }
 
-  // 동적 스타일 적용 ----------------------------------------------------------------------------->
-  baseElement.querySelectorAll("*").forEach((element) => {
-    checkAndApplyStyle(element);
-  });
-
-  var observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
+  // 7. 동적으로 생성된 요소에도 스타일 적용
+  var observer = new MutationObserver(mutations => {
+    for (let mutation of mutations) {
       if (mutation.type === "childList") {
-        mutation.addedNodes.forEach((node) => {
-          if (node.nodeType === Node.ELEMENT_NODE) {
-            checkAndApplyStyle(node);
+        for (let addedNode of mutation.addedNodes) {
+          if (addedNode.nodeType === Node.ELEMENT_NODE) {
+            fnDynamicStyle(addedNode);
           }
-        });
+        }
       }
-    });
+    }
   });
 
   observer.observe(baseElement, {
+    attributes: false,
     childList: true,
-    subtree: true
+    subtree: true,
   });
-
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  dynamicStyle();
-});
+// 0. fnMediaQuery -------------------------------------------------------------------------------->
+// 뷰포트 md 이하일 경우 카드 하단 여백이 많이 생기는 이슈를 해결하기 위함
+// 사이드바 200을 감안해서 1199px로 설정
+var fnMediaQuery = function () {
+  var mediaQuery = window.matchMedia("(max-width: 1199px)");
+  var elements = document.querySelectorAll(".cards");
+
+  // 미디어 쿼리 변경 시 실행할 함수를 정의합니다.
+  var checkMediaQuery = function() {
+    elements.forEach(function(element) {
+      if (mediaQuery.matches && element.id === "gridView") {
+        element.style.height = "88%";
+      }
+      if (mediaQuery.matches && element.id === "contentView") {
+        element.style.height = "fit-content";
+      }
+    });
+  };
+
+  // 초기 상태에서 미디어 쿼리 체크
+  checkMediaQuery();
+
+  // 미디어 쿼리 변경 시 checkMediaQuery 함수를 호출합니다.
+  mediaQuery.addEventListener("change", checkMediaQuery);
+};
+
+// ------------------------------------------------------------------------------------------------>
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", fnDynamicStyle);
+  document.addEventListener("DOMContentLoaded", fnMediaQuery);
+}
+else {
+  fnDynamicStyle();
+  fnMediaQuery();
+}
